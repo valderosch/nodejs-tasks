1b8b053bf1ee92424dc203f7290838b85fb2375f
valderosch <phonelix96@gmail.com>
2024-04-01 11:20:03 +0300

    task

--- task1/app.js
+++ task1/app.js
@@ -14,36 +14,40 @@ function roundValue(inputValue) {
 
 // Math operations function
 function operate (value1, operator, value2) {
-    let first = parseInt(value1);
-    let second = parseInt(value2);
-    let result = '0';
-    switch (operator){
-        case '+':
-            result = first + second;
-            break;
-        case '-':
-            if (first > second){
-                result = first - second;
-            }
-            else {
-                console.log('Error! First number must be bigger than second to operate this.');
-                result = '0';
-            }
-            break;
-        case '/':
-            if (first > second){
-                result = first / second;
-            }
-            else {
-                console.log('Error! First number must be bigger than second to operate this.');
-                result = '0';
-            }
-            break;
-        case '*':
-            result = first * second;
-    }
+    try {
+        let first = parseInt(value1);
+        let second = parseInt(value2);
+        let result = '0';
+        switch (operator){
+            case '+':
+                result = first + second;
+                break;
+            case '-':
+                if (first > second){
+                    result = first - second;
+                }
+                else {
+                    console.log('Error! First number must be bigger than second to operate this.');
+                    result = '0';
+                }
+                break;
+            case '/':
+                if (first > second){
+                    result = first / second;
+                }
+                else {
+                    console.log('Error! First number must be bigger than second to operate this.');
+                    result = '0';
+                }
+                break;
+            case '*':
+                result = first * second;
+        }
 
-    return roundValue(result);
+        return roundValue(result);
+    } catch (e) {
+        console.log(`Error while operating numbers. \n ${e}`);
+    }
 }
 
 // Prototypes for calling method properly
@@ -62,6 +66,6 @@ String.prototype.divide = function(value) {
 
 // Tests
 console.log("Plus: " + "140".plus("450"));
-console.log("Minus: " + "10fff00".minus("120"));
+console.log("Minus: " + "1000".minus("120"));
 console.log("Multiply: " + "5".multiply("6"));
 console.log("Divide: " + "1000".divide("25"));

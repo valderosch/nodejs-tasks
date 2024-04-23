8fcc465ac704c7f51b8f2d993c115aa29fa5f12b
valderosch <phonelix96@gmail.com>
2024-04-23 13:54:30 +0300

    composfix

--- task3/composition.js
+++ task3/composition.js
@@ -1,27 +1,64 @@
 // Point-free style and composition
 
+// Get full Username
 const getFullName = (person) => {
     return `${person.firstName} ${person.lastName}`;
 }
 
+// Unique words
+const getWordsOfText = (text) => {
+    return text.split(' ');
+}
+
+const filterUniqueValues = (values) => {
+    return values.filter((word, index, array) => array.indexOf(word) === array.lastIndexOf(word));
+}
+
+const sortAlphabetically = (values) => {
+    return values.sort();
+}
+
 const filterUniqueWords = (text) => {
-    const uniqueWordsSet = new Set(text.split(' '));
-    const uniqueWordsArray = Array.from(uniqueWordsSet).sort();
-    return uniqueWordsArray;
+    if (typeof text !== 'string') {
+        throw new Error('Input value must be a string to operate this');
+    }
+    return sortAlphabetically(filterUniqueValues(getWordsOfText(text)))
 };
 
-const getAverageGrade = (students) => students.reduce((total, student) =>
-    total + student.grades.reduce((currentValue, grade) => currentValue + grade, 0), 0) / (students.length * students[0].grades.length);
+// Average Grades
+const getTotalGrades = (students) => {
+    return students.reduce((total, student) =>
+        total + student.grades.reduce((currentValue, grade) => currentValue + grade, 0), 0);
+}
 
+const countAllGrades = (students) => {
+    return students.length * students[0].grades.length;
+}
+
+const calculateAverageGrade = (totalGrades, numberOfGrades) => {
+    return totalGrades / numberOfGrades;
+}
+
+const getAverageGrade = (students) => {
+    return calculateAverageGrade(getTotalGrades(students), countAllGrades(students));
+}
+
+// Tests
 const person = { firstName: 'Alex', lastName: 'Johnson' };
 console.log(`Person: ${getFullName(person)}`);
 
-const wordsData = 'one second one third second fifth second';
+const wordsData = 'alarm clock zebra zoo zoo lock one second one third second fifth second';
+const fakeData = ['one', 2, 22];
+
 console.log(`\nGiven: ${wordsData}\nUnique: ${filterUniqueWords(wordsData)}\n`);
+console.log(`\nGiven: ${fakeData}\nUnique: ${filterUniqueWords(fakeData)}\n`);
 
 const students = [
     { name: 'Alice', grades: [85, 90, 88] },
     { name: 'Bob', grades: [75, 80, 82] },
-    { name: 'Charlie', grades: [90, 92, 88] }
+    { name: 'Charlie', grades: [90, 92, 88] },
+    { name: 'Melanie', grades: [50, 70, 90] },
+    { name: 'Donald', grades: [55, 65, 70] },
 ];
+
 console.log(`Students average score data: ${getAverageGrade(students).toFixed(2)}`);
